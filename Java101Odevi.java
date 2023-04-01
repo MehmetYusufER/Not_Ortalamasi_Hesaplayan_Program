a87c79f7286e2815ec08b072d8d0fc19f618e3f6
Mehmet Yusuf ER <mehmetyusufder@gmail.com>
2023-04-01 12:06:36 +0300

    Initial commit

--- Java101Odevi.java
+++ Java101Odevi.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Java101ödevi {
+public class Java101Odevi {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
