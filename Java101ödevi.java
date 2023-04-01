0ce7ef2719e90d73085fb9250e7b5dc4e89541a7
Mehmet Yusuf ER <mehmetyusufder@gmail.com>
2023-04-01 11:53:24 +0300

    Inıtial Commit

---
+++ Java101ödevi.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Java101ödevi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int math, phys, chem, turk, hist, music;
+        String str;
+
+        System.out.print("Matematik notunu giriniz: ");
+        math = input.nextInt();
+
+        System.out.print("Fizik notunu giriniz: ");
+        phys= input.nextInt();
+
+        System.out.print("Kimya notunu giriniz: ");
+        chem = input.nextInt();
+
+        System.out.print("Türkçe notunu giriniz: ");
+        turk = input.nextInt();
+
+        System.out.print("Tarih notunu giriniz: ");
+        hist = input.nextInt();
+
+        System.out.print("Müzik notunu giriniz: ");
+        music = input.nextInt();
+
+        float sum = (math + phys + chem + turk + hist + music) / 6;
+
+        System.out.println("Ortalama Not " + sum);
+
+        str = sum > 60 ? "Geçti" : "Kaldı";
+        System.out.print(str);
+
+    }
+}
